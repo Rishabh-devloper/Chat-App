1e1415c28d995578a10b95d8120a1a7e8f459834
Rishabh-devloper <rishabhmishra143a@gmail.com>
2025-05-26 15:34:51 +0530

    Project is Completed

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -5,29 +5,30 @@ import { createServer } from 'http';
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server,{
-    cors:{
+const io = new Server(server, {
+    cors: {
         origin: 'http://localhost:5173',
-        
+
     }
 })
 
-export const getReciverSocketId= (userId) => {
-    return userSocketMap[userId];
-}
 
 
 
 // used for storing online users
-    const userSocketMap = {}
+const userSocketMap = {}
+export const getReciverSocketId = (userId) => {
+    return userSocketMap[userId];
+}
+
 
 
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
     const userId = socket.handshake.query.userId; // Assuming userId is sent as a query parameter
-    if(userId){
-        userSocketMap[userId] = socket.id; 
+    if (userId) {
+        userSocketMap[userId] = socket.id;
     }
     io.emit("onlineUsers", Object.keys(userSocketMap)); // Emit online users to all clients
 
@@ -40,4 +41,4 @@ io.on('connection', (socket) => {
     // You can add more event listeners here
 });
 
-export {io , server , app};
+export { io, server, app };
