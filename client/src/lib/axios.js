ba5ad56cfcc2bb8ab8d25cfe8409ca07f43aa652
Rishabh-devloper <rishabhmishra143a@gmail.com>
2025-05-28 13:28:57 +0530

    Project Complete

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // or your backend URL
+  baseURL: import.meta.env.MODE ==="development" ?"http://localhost:5000/api": "/api", // or your backend URL
   withCredentials: true, // 🔥 this is required to send/receive cookies
 });
--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ import cookieParser from "cookie-parser";
 import messageRouter from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path"
 dotenv.config();
 
+
+const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: "20mb" })); 
@@ -20,6 +23,15 @@ app.use(cors({
 
 app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRouter);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname , "../client/dist")))
+
+  app.get("*" , (req , res)=>{
+    res.sendFile(path.join(__dirname , "../client", "dist", "index.html"))
+  })
+}
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
