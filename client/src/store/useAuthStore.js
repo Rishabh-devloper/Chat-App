6547b04d853cc6363830c3a36e3c769ed2fe4a1d
Rishabh-devloper <rishabhmishra143a@gmail.com>
2025-05-28 13:38:17 +0530

    Solved all problem

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 
-const BASE_URL = import.meta.env.MODE ==="development" ?"http://localhost:5000": "/"; // Update with your server URL
+
 
 const BASE_URL = import.meta.env.MODE==="development" ? "http://localhost:5000/":"/"; // Update with your server URL
 
--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRouter from "./routes/message.route.js";
 import cors from "cors";
-<<<<<<< HEAD
 import { app, server } from "./lib/socket.js";
 import path from "path"
 dotenv.config();
@@ -13,19 +12,11 @@ dotenv.config();
 
 const __dirname = path.resolve()
 
-=======
-import { app ,  server } from "./lib/socket.js";
-import path from "path";
+=
 
 dotenv.config();
->>>>>>> 15801ae32de03da3953078ffa4fb44fb0c82fc35
+
 const PORT = process.env.PORT || 3000;
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 app.use(express.json({ limit: "20mb" })); 
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
@@ -38,7 +29,7 @@ app.use(cors({
 app.use('/api/auth', authRouter); 
 app.use('/api/messages', messageRouter);
 
-<<<<<<< HEAD
+
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname , "../client/dist")))
 
@@ -47,19 +38,9 @@ if(process.env.NODE_ENV==="production"){
   })
 }
 
-=======
-// This must come after all API routes
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-  app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
 
 
 
->>>>>>> 15801ae32de03da3953078ffa4fb44fb0c82fc35
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
