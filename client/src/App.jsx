8cd3a5115b6df591759fef4bd03a162d601223fd
Rishabh-devloper <rishabhmishra143a@gmail.com>
2025-05-28 16:18:18 +0530

    fix: improve settings page layout and theme preview functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth , onlineUsers } = useAuthStore()
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore()
   const { theme } = useThemeStore()
 
   console.log("Online Users:", onlineUsers);
@@ -36,7 +36,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
           <Route path='/signup' element={!authUser ? <SignupPage /> : <Navigate to="/" />} />
-          <Route path='/settings' element={<SettingPage />} />
+          <Route path='/settings' element={authUser ? <SettingPage /> : <Navigate to="/login" />} />
           <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
           <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
         </Routes>
